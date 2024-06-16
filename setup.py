5990daff5a00b63328751f14dc1095e667aef92c
Alexey Tochin <alexey.Tochin@Gmail.com>
2024-06-16 13:56:50 +0200

    wip

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
